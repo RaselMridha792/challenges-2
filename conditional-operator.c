26e249f4ad7dc1fd14d7bde7fbeb0a163e7d6434
RaselMridha792 <raselmridha792@gmail.com>
2025-04-28 03:31:17 +0600

    logical or operator learning

--- conditional-operator.c
+++ conditional-operator.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-      int a, b, c;
-      printf("enter your numbers");
-      scanf("%d" "%d" "%d", &a, &b, &c);
-      if (a > b && a > c)
-      {
-            printf("the biggest number is a %d :", a);
-      }else if (b > a && b > c)
-      {
-            printf("the biggest number is b %d :", b);
-      }else{
-            printf("the biggest number is c %d :", c);
+      // int a, b, c;
+      // printf("enter your numbers");
+      // scanf("%d" "%d" "%d", &a, &b, &c);
+      // if (a > b && a > c)
+      // {
+      //       printf("the biggest number is a %d :", a);
+      // }else if (b > a && b > c)
+      // {
+      //       printf("the biggest number is b %d :", b);
+      // }else{
+      //       printf("the biggest number is c %d :", c);
 
+      // }
+
+      // or operator
+      int a;
+      printf("enter your number: ");
+      scanf("%d", &a);
+      if(a == 2 || a == 4){
+            printf("yes you are enter the even number");
+      }else{
+            printf("wrong input");
       }
       return 0;
 }
